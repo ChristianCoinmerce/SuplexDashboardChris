c207e69a3b271e2488e1b233ef1bc8b4527b6876
ChristianCoinmerce <christiancoinmerce@gmail.com>
2021-03-12 16:11:06 +0100

    end week ether dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,13 +8,14 @@ use App\Http\Controllers\Controller;
 class DashboardController extends Controller
 {
     public function index()
-    {   
+    {
         //0x035f9fb6a1e22e5659e7555d211418038154b8e4
+        //0x5ce7affc1f612e0e6b2181e96a605471e66e1478
         $exchange = json_decode(file_get_contents('https://api.nomics.com/v1/currencies/ticker?key=76b6c005916b60c4a888b7f34d95ca6d&ids=ETH&interval=1d,30d&convert=EUR&per-page=100&page=1'), true);
-        $payouts1 = json_decode(file_get_contents('https://api.ethermine.org/miner/0x035f9fb6a1e22e5659e7555d211418038154b8e4/payouts/'), true);
-        $payouts = json_decode(file_get_contents('https://api.ethermine.org/miner/0x035f9fb6a1e22e5659e7555d211418038154b8e4/payouts/'), true);
-        $workers  = json_decode(file_get_contents('https://api.ethermine.org/miner/0x035f9fb6a1e22e5659e7555d211418038154b8e4/currentStats/'), true);
-        $workers1  = json_decode(file_get_contents('https://api.ethermine.org/miner/0x035f9fb6a1e22e5659e7555d211418038154b8e4/workers/'), true);
+        $payouts1 = json_decode(file_get_contents('https://api.ethermine.org/miner/0x5ce7affc1f612e0e6b2181e96a605471e66e1478/payouts/'), true);
+        $payouts = json_decode(file_get_contents('https://api.ethermine.org/miner/0x5ce7affc1f612e0e6b2181e96a605471e66e1478/payouts/'), true);
+        $workers  = json_decode(file_get_contents('https://api.ethermine.org/miner/0x5ce7affc1f612e0e6b2181e96a605471e66e1478/currentStats/'), true);
+        $workers1  = json_decode(file_get_contents('https://api.ethermine.org/miner/0x5ce7affc1f612e0e6b2181e96a605471e66e1478/workers/'), true);
         $exchange = $exchange[0];
         $worker = $workers['data'];
 
