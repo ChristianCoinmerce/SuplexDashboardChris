f3c7ee121e1e46df1a77c5bf44789cd02a0ac446
ChristianCoinmerce <christiancoinmerce@gmail.com>
2021-03-29 09:56:28 +0200

    folder cleanup

--- storage/framework/views/cb035cce3933cc6e7999e31e0962b329b83977b4.php
+++
@@ -1,6 +0,0 @@
-<?php $__env->startSection('content'); ?>
-
-
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.dashboard', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/mac/Documents/SuplexDashboardChris/resources/views/home.blade.php ENDPATH**/ ?>
